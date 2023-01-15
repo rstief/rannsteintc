2edade4bab1b7c05e4c1381fbc7ad36aff54d8f4
Nikola <niktom@web.de>
2023-01-15 20:53:09 +0100

    added logo (not positioned right currently), created a component for appbar

--- tc-rannstein-app/src/App.tsx
+++ tc-rannstein-app/src/App.tsx
@@ -1,7 +1,6 @@
-import React from 'react';
 import './App.css';
-import { AppBar, createTheme, Divider, Grid, IconButton, Menu, MenuItem, ThemeProvider, Toolbar, Typography } from '@mui/material';
-import MenuIcon from '@mui/icons-material/Menu';
+import { createTheme, Grid, ThemeProvider } from '@mui/material';
+import NavBar from './components/NavBar';
 // @ts-ignore
 import ChatBot from 'react-simple-chatbot';
 import testFlow from './chatbot-flow/test-flow';
@@ -29,88 +28,13 @@ const theme = createTheme({
 });
 
 function App() {
-  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
-  const open = Boolean(anchorEl);
-  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-    setAnchorEl(event.currentTarget);
-  };
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
-
   return (
     <ThemeProvider theme={theme}>
-      <AppBar position="static" sx={{}}>
-        <Toolbar sx={{ display: "flex", alignItems: "center", justifyContent: "space-between" }}>
-          {//todo add Logo
-          }
-          <Typography variant="h6">
-            Lilo
-          </Typography>
-          <IconButton edge="end" color="inherit" aria-label="menu" aria-controls={open ? 'basic-menu' : undefined} aria-haspopup="true"
-            aria-expanded={open ? 'true' : undefined}
-            onClick={handleClick}>
-            <MenuIcon />
-          </IconButton>
-          <Menu
-            id="basic-menu"
-            anchorEl={anchorEl}
-            open={open}
-            onClose={handleClose}
-            MenuListProps={{
-              'aria-labelledby': 'basic-button',
-            }}>
-            <MenuItem sx={{ backgroundColor: "#2D5E6E" }} onClick={undefined}>Police Resources</MenuItem>
-            <MenuItem onClick={handleClose}>
-              <Typography>
-                Closest Police Stations
-              </Typography>
-            </MenuItem>
-            <MenuItem onClick={handleClose}>
-              <Typography>
-                Reporting Crime & What To Do
-              </Typography>
-            </MenuItem>
-            <Divider />
-
-            <MenuItem sx={{ backgroundColor: "#2D5E6E" }} onClick={undefined}>Legal Resources</MenuItem>
-            <MenuItem onClick={handleClose}>
-              <Typography>
-                Closest Police Stations
-              </Typography>
-            </MenuItem>
-            <MenuItem onClick={handleClose}>
-              <Typography>
-                Reporting Crime & What To Do
-              </Typography>
-            </MenuItem>
-            <Divider />
-
-            <MenuItem sx={{ backgroundColor: "#2D5E6E" }} onClick={undefined}>Community Resources</MenuItem>
-            <MenuItem onClick={handleClose}>
-              <Typography>
-                Support Groups & Hotlines
-              </Typography>
-            </MenuItem>
-          </Menu>
-          {/* <Grid container sx={{ display: "flex", alignItems: "center", justifyContent: "space-between" }}>
-            <Grid item>
-              <Typography variant="h6">
-                My App
-              </Typography>
-            </Grid>
-            <Grid item>
-              <IconButton edge="end" color="inherit" aria-label="menu">
-                <MenuIcon />
-              </IconButton>
-            </Grid>
-          </Grid> */}
-        </Toolbar>
-      </AppBar>
-      <Grid container sx={{ justifyContent: "center", alignItems: "stretch", height: "100%" }}>
+      <NavBar />
+      <Grid container sx={{ justifyContent: 'center', alignItems: 'stretch', height: '100%' }}>
         {/* empty side left*/}
         {/* add chatbot */}
-        <Grid item xs={12} md={6} sx={{ backgroundColor: "green" }}>
+        <Grid item xs={12} md={6} sx={{ backgroundColor: 'green' }}>
           <ChatBot steps={testFlow} />
         </Grid>
         {/* empty side right */}
---
+++ tc-rannstein-app/src/components/NavBar.tsx
@@ -0,0 +1,77 @@
+import { AppBar, Box, Divider, IconButton, Menu, MenuItem, Toolbar, Typography } from "@mui/material"
+import MenuIcon from '@mui/icons-material/Menu';
+import logo from '../assets/logo.svg'
+import React from "react";
+
+
+const NavBar = () => {
+    const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+    const open = Boolean(anchorEl);
+    const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+      setAnchorEl(event.currentTarget);
+    };
+    const handleClose = () => {
+      setAnchorEl(null);
+    };
+  
+    return(
+        <AppBar position="static" sx={{}}>
+        <Toolbar sx={{ display: "flex", alignItems: "center", justifyContent: "space-between" }}>
+          <Box sx={{flexDirection: "row", flexWrap: 'nowrap', justifyContent: "flex-start"}}>
+            <img src={logo} className="App-logo" alt="logo" />
+            <Typography variant="h6">
+              Lilo
+            </Typography>
+          </Box>
+          <IconButton color="inherit" aria-label="menu" aria-controls={open ? 'basic-menu' : undefined} aria-haspopup="true"
+            aria-expanded={open ? 'true' : undefined}
+            onClick={handleClick}>
+            <MenuIcon />
+          </IconButton>
+          <Menu
+            id="basic-menu"
+            anchorEl={anchorEl}
+            open={open}
+            onClose={handleClose}
+            MenuListProps={{
+              'aria-labelledby': 'basic-button',
+            }}>
+            <MenuItem sx={{ backgroundColor: "#2D5E6E" }} onClick={undefined}>Police Resources</MenuItem>
+            <MenuItem onClick={handleClose}>
+              <Typography>
+                Closest Police Stations
+              </Typography>
+            </MenuItem>
+            <MenuItem onClick={handleClose}>
+              <Typography>
+                Reporting Crime & What To Do
+              </Typography>
+            </MenuItem>
+            <Divider />
+
+            <MenuItem sx={{ backgroundColor: "#2D5E6E" }} onClick={undefined}>Legal Resources</MenuItem>
+            <MenuItem onClick={handleClose}>
+              <Typography>
+                Closest Police Stations
+              </Typography>
+            </MenuItem>
+            <MenuItem onClick={handleClose}>
+              <Typography>
+                Reporting Crime & What To Do
+              </Typography>
+            </MenuItem>
+            <Divider />
+
+            <MenuItem sx={{ backgroundColor: "#2D5E6E" }} onClick={undefined}>Community Resources</MenuItem>
+            <MenuItem onClick={handleClose}>
+              <Typography>
+                Support Groups & Hotlines
+              </Typography>
+            </MenuItem>
+          </Menu>
+        </Toolbar>
+      </AppBar>
+    )
+}
+
+export default NavBar;

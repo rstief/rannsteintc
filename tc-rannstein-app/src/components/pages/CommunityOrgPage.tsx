3ec7b5d6354bd2d77a4d8cb10fffa64e8b27cb5f
Nikola <niktom@web.de>
2023-01-21 04:51:29 +0100

    now also working on detail community page

--- tc-rannstein-app/src/components/pages/CommunityOrgPage.tsx
+++ tc-rannstein-app/src/components/pages/CommunityOrgPage.tsx
@@ -1,36 +1,59 @@
-import { Box, Typography } from '@mui/material';
+import { Box, Chip, Typography } from '@mui/material';
 import ResourcesBar from '../ResourcesBar';
 import communityfriendship from '../../assets/communityfriendship.svg'
 import { useParams } from 'react-router-dom';
 import Divider from '@mui/material/Divider';
 import organisationList from '../../data/organizations.json'
+import CallIcon from '@mui/icons-material/Call';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
+import LanguageIcon from '@mui/icons-material/Language';
+
+interface org {
+    id: String
+    shortName: string,
+    fullName: string,
+    description: string,
+    state: string,
+    address: {
+        street: string,
+        city: string
+    },
+    phone: string,
+    link: string,
+    tags: string[],
+    services: string[]
+}
 
 const CommunityOrgPage = () => {
 
     let { orgId } = useParams();
-
+    const detailOrg: org | any = organisationList.find(item => item.id === orgId)
     console.log(orgId);
-
     return (
         <Box>
             <ResourcesBar innerRef='/lilo/communityresources' name='Community Resources'></ResourcesBar>
             <Box display={'flex'} height='20%' marginLeft='8px' marginRight='8px' mb='10px' sx={{ justifyContent: 'space-between' }}>
-                <img src={communityfriendship} className='communityfriendimage' alt='intro' />
-                <Typography m='8px' fontSize='13px'>
-                    Fachstelle gegen sexualisierte gewalt an Frauen {orgId}
+                <img src={communityfriendship} className='communityfriendimage' alt='intro' width='15%' />
+                <Typography width='85%' m='8px' fontSize='13px' >
+                    {detailOrg.fullName}
                 </Typography>
             </Box>
-            <Divider sx={{marginLeft: '30px', marginRight:'30px'}} />
+            <Divider sx={{ marginLeft: '30px', marginRight: '30px' }} />
             <Box height='50%' ml='20px' mr='20px' pb='5px' pl='10px' pr='10px' sx={{ boxShadow: 4, borderRadius: '10px' }}>
+                <Box display={'flex'} justifyContent={'space-evenly'}>
+                    <Chip sx={{ justifyContent:'center' }} icon={<LocationOnIcon />} />
+                    <Chip icon={<CallIcon />} />
+                    <Chip icon={<LanguageIcon />} />
+                </Box>
                 <Typography m='8px' fontSize='13px'>
-                    Location
+                    {detailOrg.address.street + detailOrg.address.city}
                 </Typography>
                 <Typography m='8px' fontSize='13px'>
-                    Telefon
+                    {detailOrg.phone}
                 </Typography>
-                <Box pr='5px' pl='5px' sx={{border:1, borderColor:'black', borderRadius: '10px'}}>
+                <Box pr='5px' pl='5px' sx={{ border: 1, borderColor: 'black', borderRadius: '10px' }}>
                     <Typography m='5px' fontSize='10px'>
-                        Lara is a rape crisis and counselling centre that offers assistance to women and girls over the age of 14 who have been raped, sexually abused, and/or sexually harassed
+                        {detailOrg.description}
                     </Typography>
                 </Box>
             </Box>

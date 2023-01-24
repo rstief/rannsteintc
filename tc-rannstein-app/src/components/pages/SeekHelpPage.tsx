3d2852122d2a2112a481217c6a9a390094fe4002
Raphael Stief <raphael.stief@gmail.com>
2023-01-24 11:43:08 +0100

    Added Header for Seek Help

--- tc-rannstein-app/src/components/pages/SeekHelpPage.tsx
+++ tc-rannstein-app/src/components/pages/SeekHelpPage.tsx
@@ -1,14 +1,17 @@
-import {Box} from '@mui/material';
+import {Box, Typography} from '@mui/material';
 // @ts-ignore
-import ChatBot from 'react-simple-chatbot';
-import InteractionBar from '../InteractionBar';
 import ConfiguredChatBot from "../ConfiguredChatBot";
 import {seekHelpFlow} from "../../chatbot-flow/flow";
+import {useTranslation} from "react-i18next";
 
 const SeekHelpPage = () => {
+    const { t } = useTranslation();
+
   return (
       <Box sx={{display: 'flex', flexDirection: 'column', height:'calc(100% - 56px)'}}>
-          <InteractionBar />
+          <Typography align='center' fontWeight='bold' mt='10px' mb='10px' color='#263238'>
+              {t('seekhelppageexp')}
+          </Typography>
           <ConfiguredChatBot steps={seekHelpFlow} width="100%" height="100%"/>
       </Box>
   )
